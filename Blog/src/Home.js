c86be9d1d3d785b11f46203045eaf0d90227b8da
Gabe Smith <gabe.smith@coop.org>
2022-01-03 16:46:40 -0600

    added hooks

--- Blog/src/Home.js
+++ Blog/src/Home.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [number, setNumber] = useState(6);
+
+    const handleClick = () => {
+        setNumber(Math.round(Math.random() * 45))
+    }
+
     return ( 
         <div className="home">
-            <h2>Home</h2>
+            <h2>Homepage</h2>
+            <h1>{number}</h1>
+            <button onClick={() => handleClick()}>Click me!</button>
         </div>
      );
 }
