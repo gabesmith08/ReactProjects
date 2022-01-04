88f7736d17a3eb4da50a1f578d555b1158c4d230
Gabe Smith <gabe.smith@coop.org>
2022-01-04 17:08:10 -0600

    added blog channels

--- Blog/src/Home.js
+++ Blog/src/Home.js
@@ -1,17 +1,20 @@
 import { useState } from "react";
 
 const Home = () => {
-    const [number, setNumber] = useState(6);
-
-    const handleClick = () => {
-        setNumber(Math.round(Math.random() * 45))
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'wadup', author: 'Gabe', id: 1 },
+        { title: 'Welcome party!', body: 'hey der', author: 'Gabe', id: 2 },
+        { title: 'Web dev top tips', body: 'yooo', author: 'Gabe', id: 3 },
+    ]);
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <h1>{number}</h1>
-            <button onClick={() => handleClick()}>Click me!</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by: { blog.author}</p>
+                </div>
+            ))}
         </div>
      );
 }
